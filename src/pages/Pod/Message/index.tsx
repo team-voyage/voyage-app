7deff22d9dda4e53de225b45caf9c8625dc4ba5b
최재민 <admin@chicken-moo.com>
2023-08-20 06:07:50 +0900

    feat: modal

--- src/pages/Pod/Message/index.tsx
+++ src/pages/Pod/Message/index.tsx
@@ -1,17 +1,19 @@
 import React, { Fragment, useState } from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Platform, SafeAreaView, StatusBar, StyleSheet, Text, View, TouchableOpacity, TextInput } from "react-native";
+import { Platform, SafeAreaView, StatusBar, StyleSheet, Text, View, TouchableOpacity, TextInput, Image } from "react-native";
 
-import colors from "@/utils/colors";
+import colors, { opacity } from "@/utils/colors";
 
 import { PodStackParamList } from "../types";
 import UndoIcon from "@/assets/icons/undo.svg";
 import CommunityIcon from "@/assets/icons/community.svg";
+import CloseIcon from "@/assets/icons/xx.svg";
 import SendIcon from "@/assets/icons/send.svg";
 import styles from "./styles";
 
 type props = NativeStackScreenProps<PodStackParamList, "Message">;
 const Message = ({ navigation: messageNavigation, route }: props) => {
+  const [modal, setModal] = useState(false);
 
   const [messages, setMessages] = useState<{
     sender: string;
@@ -61,20 +63,88 @@ const Message = ({ navigation: messageNavigation, route }: props) => {
 
   return (
     <Fragment>
+      {
+        modal && (
+          <View style={styles.modal}>
+            <View style={styles.modalBackground}>
+              <View style={styles.modalTitle}>
+                <View style={{ width: 24, height: 24 }} />
+                <Text style={styles.modalTitleText}>member</Text>
+                <TouchableOpacity 
+                  onPress={() => setModal(false)}
+                  hitSlop={{ top: 16, bottom: 16, left: 16, right: 16 }}
+                >
+                  <CloseIcon width={24} height={24} />
+                </TouchableOpacity>
+              </View>
+
+              <View style={styles.modalContent}>
+                {
+                  [
+                    {
+                      name: "Kim Hyung Seok (me)",
+                      Image: require("@/assets/images/kim.png")
+                    },
+                    {
+                      name: "Choi Jae Min",
+                      Image: require("@/assets/images/choi.png")
+                    },
+                    {
+                      name: "Lim Ji Hoon",
+                      Image: require("@/assets/images/lim.png")
+                    }
+                  ].map((member, i) => (
+                    <View style={styles.mdcontent} key={i}>
+                      <View style={styles.leftSide}>
+                        <Image 
+                          style={styles.profiles} 
+                          source={member.Image} 
+                        />
+                        <Text style={styles.name}>{member.name}</Text>
+                      </View>
+                      <View style={styles.rightSide}>
+                        <Text style={styles.arrived}>arrived</Text>
+                      </View>
+                    </View>
+                  ))
+                }
+                <View style={styles.mdcontent}>
+                  <View style={styles.leftSide}>
+                    <Image 
+                      style={styles.profiles} 
+                      source={require("@/assets/images/pp.png")} 
+                    />
+                    <Text style={{...styles.name, opacity: .3}}>Find a new member</Text>
+                  </View>
+                  <View style={styles.rightSide}>
+                    <Text style={{...styles.arrived, opacity: 0}}>arrived</Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </View>
+        )
+      }
       <SafeAreaView style={styles.statusBar} />
       <SafeAreaView style={styles.container}>
         <View style={styles.background}>
           <SafeAreaView style={styles.safeareaview}>
             <View style={styles.title}>
-              <TouchableOpacity onPress={() => {
-                messageNavigation.navigate("Main");
-              }}>
+              <TouchableOpacity 
+                onPress={() => {
+                  messageNavigation.navigate("Main");
+                }}
+                hitSlop={{ top: 16, bottom: 16, left: 16, right: 16 }}
+              >
                 <UndoIcon />
               </TouchableOpacity>
               <Text style={styles.titleText}> { route.params.name } </Text>
-              <TouchableOpacity onPress={() => {
-                // messageNavigation.goBack();
-              }}>
+              <TouchableOpacity 
+                onPress={() => {
+                  setModal(true);
+                }}
+                hitSlop={{ top: 16, bottom: 16, left: 16, right: 16 }}
+              >
                 <CommunityIcon />
               </TouchableOpacity>
             </View>
@@ -94,7 +164,8 @@ const Message = ({ navigation: messageNavigation, route }: props) => {
                   style={styles.textInput}
                   value={message}
                   onChangeText={(text) => setMessage(text)}
-                  placeholder="아무거나 입력해주세요."
+                  placeholder="Message..."
+                  placeholderTextColor={opacity(colors.black, .5)}
                 />
                 <TouchableOpacity onPress={() => {
 
